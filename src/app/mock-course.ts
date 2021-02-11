ac53198be6bc6daae1c9318e8e02e74378132bd2
Ramjeet89 <ramjeet.mahto77@gmail.com>
2021-02-11 08:50:00 +0530

    Developed sample angular application

---
+++ src/app/mock-course.ts
@@ -0,0 +1,10 @@
+import { from } from "rxjs";
+import { Course } from './course'
+
+export const COURSES:Course[]=[
+    {id:1,name:"C++"},
+    {id:2,name:"JAVA"},
+    {id:3,name:".Net"} ,
+    {id:4,name:"Spring"} ,
+    {id:5,name:"Hibernate"},
+    {id:6,name:"Angular"}];
